9c91c1a9ecf92fb3dfc767bf78a4fa02ec8dcb86
dsantacruz1212 <danielsantacruz.1212@gmail.com>
2023-10-23 20:32:07 -0600

    commit #1

---
+++ components/SearchResults.js
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { StyleSheet, View, Text, TextInput, Dimensions, Button , TouchableOpacity } from "react-native";
+import { Icon } from "react-native-elements";
+
+const SearchResult= () => {
+    return (
+
+<Text>fsdfsdfsd</Text>
+        )
+    };
+
+    const styles = StyleSheet.create({
+
+
+
+    });
+    export default SearchResult;
